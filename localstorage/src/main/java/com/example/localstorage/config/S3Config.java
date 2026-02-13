b7d81f9004483d0f52874e61b1ed1bdcf2f323cf
rpratap2111 <rpratap2111@gmail.com>
2026-02-13 18:32:26 +0530

    Changed S3Config.java

--- localstorage/src/main/java/com/example/localstorage/config/S3Config.java
+++ localstorage/src/main/java/com/example/localstorage/config/S3Config.java
@@ -2,27 +2,17 @@ package com.example.localstorage.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
 
-        @Bean
-        public S3Client s3Client(
-                        S3Properties props) {
-                String accessKey = props.getAccessKey() != null ? props.getAccessKey() : "dummy-access-key";
-                String secretKey = props.getSecretKey() != null ? props.getSecretKey() : "dummy-secret-key";
-
-                return S3Client.builder()
-                                .region(Region.of(props.getRegion()))
-                                .credentialsProvider(
-                                                StaticCredentialsProvider.create(
-                                                                AwsBasicCredentials.create(
-                                                                                accessKey,
-                                                                                secretKey)))
-                                .build();
-        }
+    @Bean
+    public S3Client s3Client(S3Properties props) {
+        return S3Client.builder()
+                .region(Region.of(props.getRegion()))
+                .build();   
+    }
 }
